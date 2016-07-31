bf60d992cce3b98848832d0d56e648b9f330af23
Kristof Cools <kristof.cools@nottingham.ac.uk>
2016-07-31 15:47:41 +0100

    Initial commit

--- src/WiltonInts84.jl
+++ src/WiltonInts84.jl
@@ -1,5 +1,164 @@
 module WiltonInts84
 
-# package code goes here
+include("mutuple.jl")
+
+
+"""
+    oddints!(I, a, b, p, h Val{N})
+
+computes the integrals
+
+```math
+I_{n} = \frac{p}{n+2} \int_a^b \frac{(s^2+p^2+h^2)^{(n+2)/2}}{s^2+p^2} ds
+```
+
+for ``n`` in `-3:2:N`. `N` is required to be odd.
+
+**note**: The first element of the returned array is in fact ``h I_{-3}``. This
+is done to avoid singularities when ``h=0``.
+"""
+function oddints!{N}(I, a, b, p, h, ::Type{Val{N}})
+
+    @assert N ≥ -3
+    @assert length(I) == N+3
+
+    q2 = p^2+h^2; q = √q2
+    K = zeros(I)
+    K[1] += -atan((h*b)/(p*sqrt(b^2+q2))) + atan((h*a)/(p*sqrt(a^2+q2)))
+    J = log(b/q + √(1+(b/q)^2)) - log(a/q + √(1+(a/q)^2))
+    for i in 2 : 2 : length(I)
+        n = i-3
+        Kp = (i == 2) ? K[1]/h : K[i-2]
+        # Compute I_{n} using J_{n} and I_{n+1}
+        K[i] += p/(n+2)*(q)^(n+1)*J + n/(n+2)*h^2*Kp
+        # Now compute J_{n+2}
+        J = (b/q*√(1+(b/q)^2)^(n+2) - a/q*√(1+(a/q)^2)^(n+2) + (n+2)*J) / (n+3)
+    end
+
+    for i in eachindex(K) I[i] += K[i] end
+
+end
+
+
+function evenints!{N}(I, a, b, p, h, UB::Type{Val{N}})
+
+    @assert N ≥ -3
+    @assert length(I) == N+3
+    M = div(N,2)
+
+    pa = zeros(typeof(a), N+2); pa[1] = 1;
+    pb = zeros(typeof(a), N+2); pb[1] = 1;
+    pp = zeros(typeof(a), M+1); pp[1] = 1;
+    hp = zeros(typeof(a), M+1); hp[1] = 1;
+
+    # compute the required powers of a, b, and p
+    for i in 2:length(pa) pa[i] = a * pa[i-1] end
+    for i in 2:length(pb) pb[i] = b * pb[i-1] end
+    for i in 2:length(pp) pp[i] = p*p * pp[i-1] end
+    for i in 2:length(pp) hp[i] = h*h * hp[i-1] end
+
+    # get the binomial coeffs
+    bn = binomial(N+1)
+
+    # compute the degree independent coefficients (see notebook for details)
+    Ca = ckps(pp,pa,bn,UB)
+    Cb = ckps(pp,pb,bn,UB)
+
+    # now put the actual integrals together
+    for i in 3 : 2 : length(I)
+        n = i-3
+        @assert iseven(n)
+        m = div(n,2)
+        α = p/(n+2)
+        for k = 1 : m+1
+            I[i] += α * bn[k+1,m+2] * hp[m-k+2] * (Cb[k] - Ca[k])
+        end
+    end
+
+
+    return I
+end
+
+
+function ckps{N}(p,s,bn,::Type{Val{N}})
+
+
+    m = div(N,2)
+    C = zeros(eltype(p), m+1)
+    for k in 1 : m+1
+        for l in 0 : k-1
+            F1 = bn[l+1,k]
+            F2 = s[2l+2]
+            F3 = p[k-l]
+            C[k] += bn[l+1,k]*s[2l+2]*p[k-l]/(2l+1)
+        end
+    end
+
+    return C
+end
+
+
+"""
+    binomial(N)
+
+Compute the table of binomial coefficients up to (N+1,.)
+"""
+function binomial(N)
+    bn = zeros(Int, N+2, N+2)
+    for n in 0:N+1 (j = n+1; bn[1,j] = 1) end
+    for n in 1:N+1
+        j = n+1
+        for k in 1:n
+            i = k+1
+            bn[i,j] = bn[i-1,j-1] + bn[i,j-1]
+        end
+    end
+    return bn
+end
+
+
+function wiltonints{N}(v1,v2,v3,x,UB::Type{Val{N}})
+
+    n = cross(v1-v3, v2-v3)
+    n /= norm(n)
+
+    h = dot(x-v1,n)
+    ξ = x - h*n
+    inside = true
+
+    I = zeros(eltype(x), N+3)
+
+    # compute the contributions from the edges of the triangle
+    Va = (v1,v2,v3)
+    Vb = (v2,v3,v1)
+    for i in 1:3
+        va = Va[i]
+        vb = Vb[i]
+
+        t = vb - va
+        t /= norm(t)
+        m = cross(t, n)
+        p = dot(va-ξ,m)
+        if p < 0
+            inside = false
+        end
+        sa = dot(va-ξ,t)
+        sb = dot(vb-ξ,t)
+
+        oddints!(I, sa, sb, p, h, UB)
+        evenints!(I, sa, sb, p, h, UB)
+    end
+
+    # add to that the contributions of the singularity region
+    d = abs(h)
+    α = inside ? 2π : 0.0
+    I[1] += α*sign(h)
+    for n in -1:2:N
+        (i = n+3; I[i] += -α * d^(n+2) / (n+2))
+    end
+
+    return I
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,58 @@
 using WiltonInts84
 using Base.Test
 
+# if Pkg.installed("FixedSizeArrays") == nothing
+#     Pkg.add("FixedSizeArrays")
+# end
+
+using FixedSizeArrays
+using FastGaussQuadrature
+
 # write your own tests here
-@test 1 == 1
+WI = WiltonInts84
+
+v1 = Vec(0.0, 0.0, 0.0)
+v2 = Vec(1.0, 0.0, 0.0)
+v3 = Vec(0.0, 1.0, 0.0)
+n = normalize(cross(v1-v3,v2-v3))
+x = (v1 + v2 + v3)/3 + 20n
+
+function legendre(n,a,b)
+    x,w = FastGaussQuadrature.gausslegendre(n)
+    w *= (b-a)/2
+    x = a+(x+1)*(b-a)/2
+    return x, w
+end
+
+function dblquadints{N}(v1,v2,v3,x,::Type{Val{N}})
+    G = 20
+    s, w = legendre(G, 0.0, 1.0)
+    t1 = v1-v3
+    t2 = v2-v3
+    n = cross(t1,t2)
+    a2 = norm(n)
+    d = dot(x-v1,n)/a2
+    I = zeros(eltype(x),N+3)
+    for g in 1:G
+        u = s[g]
+        for h in 1:G
+            v = (1-u)*s[h]
+            y = v3 + u*t1 + v*t2
+            R = norm(x-y)
+            j = a2 * w[g] * w[h] * (1-u)
+            I[1] += j * d / R^3
+            for n = -1 : N
+                I[n+3] += j * R^n
+            end
+        end
+    end
+    return I
+end
+
+I = WI.wiltonints(v1,v2,v3,x,Val{7})
+J = dblquadints(v1,v2,v3,x,Val{7})
+maximum(abs(I-J)./abs(J)) < 1.0e-10
+
+x = (1-1.5)*v1 + 1.5*v3 + 20*n
+I = WI.wiltonints(v1,v2,v3,x,Val{7})
+J = dblquadints(v1,v2,v3,x,Val{7})
