693b59eead9be4e2abcc7ddfad92b0104caf343b
Kristof Cools <kristof.cools@nottingham.ac.uk>
2016-08-02 14:19:03 +0100

    consolidated computation for even and odd. Expanded testing.

--- src/WiltonInts84.jl
+++ src/WiltonInts84.jl
@@ -3,118 +3,49 @@ module WiltonInts84
 include("mutuple.jl")
 
 
-"""
-    oddints!(I, a, b, p, h Val{N})
+function allints!{N}(I, a, b, p, h, ::Type{Val{N}})
 
-computes the integrals
-
-```math
-I_{n} = \frac{p}{n+2} \int_a^b \frac{(s^2+p^2+h^2)^{(n+2)/2}}{s^2+p^2} ds
-```
-
-for ``n`` in `-3:2:N`. `N` is required to be odd.
-
-**note**: The first element of the returned array is in fact ``h I_{-3}``. This
-is done to avoid singularities when ``h=0``.
-"""
-function oddints!{N}(I, a, b, p, h, ::Type{Val{N}})
-
-    @assert N ≥ -3
+    @assert N ≥ 3
     @assert length(I) == N+3
 
-    q2 = p^2+h^2; q = √q2
+    h2 = h^2
+    q2 = p^2+h2
+    q = sqrt(q2)
+
     K = zeros(I)
-    K[1] += sign(h) * atan( (p*b) / (q2 + abs(h)*√(b^2+q2)) )
-    K[1] -= sign(h) * atan( (p*a) / (q2 + abs(h)*√(a^2+q2)) )
-    J = log(b/q + √(1+(b/q)^2)) - log(a/q + √(1+(a/q)^2))
-    for i in 2 : 2 : length(I)
-        n = i-3
-        Kp = (i == 2) ? K[1]/h : K[i-2]
-        # Compute I_{n} using J_{n} and I_{n+1}
-        K[i] += p/(n+2)*(q)^(n+1)*J + n/(n+2)*h^2*Kp
-        # Now compute J_{n+2}
-        J = (b/q*√(1+(b/q)^2)^(n+2) - a/q*√(1+(a/q)^2)^(n+2) + (n+2)*J) / (n+3)
+    J = zeros(eltype(I),2)
+
+    # n = -3
+    if p == 0
+        K[1] = 0
+    else
+        K[1] += sign(h) * atan( (p*b) / (q2 + abs(h)*√(b^2+q2)) )
+        K[1] -= sign(h) * atan( (p*a) / (q2 + abs(h)*√(a^2+q2)) )
+    end
+
+    if q2 == zero(typeof(q2))
+        J[1] = (b > 0) ? log(b/a) : log(a/b)
+    else
+        J[1] = log(b + sqrt(b^2 + q2)) - log(a + sqrt(a^2 + q2))
+    end
+
+    # n = -1
+    K[2] = p*J[1] - h*K[1]
+    J[1] = (b*√(b^2+q2) - a*√(a^2+q2) + q2*J[1])/2
+
+    # n = 0
+    K[3] += 0.5*b*p
+    K[3] -= 0.5*a*p
+    J[2] = ((b*(b^2+q2)+2*q2*b) - (a*(a^2+q2)+2*q2*a))/3
+
+    # n >= 1
+    for i in 4 : length(K)
+        n = i - 3; j = mod1(n,2)
+        K[i] = p/(n+2)*J[j] + n/(n+2)*h2*K[i-2]
+        J[j] = (b*√(b^2+q2)^(n+2) - a*√(a^2+q2)^(n+2) + (n+2)*q2*J[j])/(n+3)
     end
 
     for i in eachindex(K) I[i] += K[i] end
-
-end
-
-
-function evenints!{N}(I, a, b, p, h, UB::Type{Val{N}})
-
-    @assert N ≥ -3
-    @assert length(I) == N+3
-    M = div(N,2)
-
-    pa = zeros(typeof(a), N+2); pa[1] = 1;
-    pb = zeros(typeof(a), N+2); pb[1] = 1;
-    pp = zeros(typeof(a), M+1); pp[1] = 1;
-    hp = zeros(typeof(a), M+1); hp[1] = 1;
-
-    # compute the required powers of a, b, and p
-    for i in 2:length(pa) pa[i] = a * pa[i-1] end
-    for i in 2:length(pb) pb[i] = b * pb[i-1] end
-    for i in 2:length(pp) pp[i] = p*p * pp[i-1] end
-    for i in 2:length(pp) hp[i] = h*h * hp[i-1] end
-
-    # get the binomial coeffs
-    bn = binomial(N+1)
-
-    # compute the degree independent coefficients (see notebook for details)
-    Ca = ckps(pp,pa,bn,UB)
-    Cb = ckps(pp,pb,bn,UB)
-
-    # now put the actual integrals together
-    for i in 3 : 2 : length(I)
-        n = i-3
-        @assert iseven(n)
-        m = div(n,2)
-        α = p/(n+2)
-        for k = 1 : m+1
-            I[i] += α * bn[k+1,m+2] * hp[m-k+2] * (Cb[k] - Ca[k])
-        end
-    end
-
-
-    return I
-end
-
-
-function ckps{N}(p,s,bn,::Type{Val{N}})
-
-
-    m = div(N,2)
-    C = zeros(eltype(p), m+1)
-    for k in 1 : m+1
-        for l in 0 : k-1
-            F1 = bn[l+1,k]
-            F2 = s[2l+2]
-            F3 = p[k-l]
-            C[k] += bn[l+1,k]*s[2l+2]*p[k-l]/(2l+1)
-        end
-    end
-
-    return C
-end
-
-
-"""
-    binomial(N)
-
-Compute the table of binomial coefficients up to (N+1,.)
-"""
-function binomial(N)
-    bn = zeros(Int, N+2, N+2)
-    for n in 0:N+1 (j = n+1; bn[1,j] = 1) end
-    for n in 1:N+1
-        j = n+1
-        for k in 1:n
-            i = k+1
-            bn[i,j] = bn[i-1,j-1] + bn[i,j-1]
-        end
-    end
-    return bn
 end
 
 
@@ -146,8 +77,7 @@ function wiltonints{N}(v1,v2,v3,x,UB::Type{Val{N}})
         sa = dot(va-ξ,t)
         sb = dot(vb-ξ,t)
 
-        oddints!(I, sa, sb, p, h, UB)
-        evenints!(I, sa, sb, p, h, UB)
+        allints!(I, sa, sb, p, h, UB)
     end
 
     return I
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,10 @@ using FastGaussQuadrature
 # write your own tests here
 WI = WiltonInts84
 
-v1 = Vec(0.0, 0.0, 0.0)
-v2 = Vec(1.0, 0.0, 0.0)
-v3 = Vec(0.0, 1.0, 0.0)
+v1 = Vec(1.0, 0.0, 0.0)
+v2 = Vec(0.0, 1.0, 0.0)
+v3 = Vec(0.0, 0.0, 0.0)
 n = normalize(cross(v1-v3,v2-v3))
-x = (v1 + v2 + v3)/3 + 20n
 
 function legendre(n,a,b)
     x,w = FastGaussQuadrature.gausslegendre(n)
@@ -20,15 +19,39 @@ function legendre(n,a,b)
     return x, w
 end
 
-function dblquadints{N}(v1,v2,v3,x,::Type{Val{N}})
+function dblquadints{N}(v1,v2,v3,x,T::Type{Val{N}})
+
+    I = zeros(eltype(x),N+3)
+    n = normalize(cross(v1-v3,v2-v3))
+    ξ = x - dot(x-v1,n)*n
+
+    σ = sign(dot(cross(v1-ξ,v2-ξ),n))
+    K = σ * dblquadints1(ξ,v1,v2,x,T)
+    # correctionn required for \int h/R^{-3} because
+    # the definition of h is linked to triangle orientation
+    K[1] *= σ; I += K
+
+    σ = sign(dot(cross(v2-ξ,v3-ξ),n))
+    K = σ * dblquadints1(ξ,v2,v3,x,T)
+    K[1] *= σ; I += K
+
+    σ = sign(dot(cross(v3-ξ,v1-ξ),n))
+    K = σ * dblquadints1(ξ,v3,v1,x,T)
+    K[1] *= σ; I += K
+
+    return I
+end
+
+function dblquadints1{N}(v1,v2,v3,x,::Type{Val{N}})
     G = 20
     s, w = legendre(G, 0.0, 1.0)
     t1 = v1-v3
     t2 = v2-v3
     n = cross(t1,t2)
-    a2 = norm(n)
-    d = dot(x-v1,n)/a2
     I = zeros(eltype(x),N+3)
+    a2 = norm(n)
+    a2 < eps(eltype(x)) && return I
+    d = dot(x-v1,n)/a2
     for g in 1:G
         u = s[g]
         for h in 1:G
@@ -45,25 +68,93 @@ function dblquadints{N}(v1,v2,v3,x,::Type{Val{N}})
     return I
 end
 
-# projection inside the triangle
-I = WI.wiltonints(v1,v2,v3,x,Val{7})
-J = dblquadints(v1,v2,v3,x,Val{7})
-@test maximum(abs(I-J)./abs(J)) < 1.0e-10
+X = [
+    (v1 + v2 + v3)/3               + 20n, # h > 0, inside
+    (1-1.5)v1 + 1.5v3              + 20n, # h > 0, on extension [v1,v3]
+    0.5v1 + 0.5v3                  + 20n, # h > 0, on the interior of [v1,v3]
+    -0.5v1 + 0.5v2 + (1-0.5-0.5)v3 + 20n, # h > 0, outside
+    v2                             + 20n, # h > 0, on top of v2
 
-# projection on a triangle edge
-x = (1-1.5)*v1 + 1.5*v3 + 20*n
-I = WI.wiltonints(v1,v2,v3,x,Val{7})
-J = dblquadints(v1,v2,v3,x,Val{7})
-@test maximum(abs(I-J)./abs(J)) < 1.0e-10
+    (v1 + v2 + v3)/3               - 20n, # h < 0, inside
+    (1-1.5)v1 + 1.5v3              - 20n, # h < 0, on extension [v1,v3]
+    0.5v1 + 0.5v3                  - 20n, # h < 0, on the interior of [v1,v3]
+    -0.5v1 + 0.5v2 + (1-0.5-0.5)v3 - 20n, # h < 0, outside
+    v2                             - 20n, # h < 0, on top of v2
 
-# projection outside of the triangle
-x = Vec(-0.5,0.5,20.0)
-I = WI.wiltonints(v1,v2,v3,x,Val{7})
-J = dblquadints(v1,v2,v3,x,Val{7})
-@test maximum(abs(I-J)./abs(J)) < 1.0e-10
+    (v1 + v2 + v3)/3               - 0n,  # h = 0, inside
+    (1-1.5)v1 + 1.5v3              - 0n,  # h = 0, on extension [v1,v3]
+    -0.5v1 + 0.5v2 + (1-0.5-0.5)v3 - 0n,  # h = 0, outside
+]
 
-# projection on a vertex
-x = Vec(0.0,1.0,20.0)
-I = WI.wiltonints(v1,v2,v3,x,Val{7})
-J = dblquadints(v1,v2,v3,x,Val{7})
-@test maximum(abs(I-J)./abs(J)) < 1.0e-10
+τ = [
+    1e-10,
+    1e-10,
+    1e-10,
+    1e-10,
+    1e-10,
+
+    1e-10,
+    1e-10,
+    1e-10,
+    1e-10,
+    1e-10,
+
+    1e-7,
+    1e-10,
+    1e-10,
+]
+
+function nearlyequal{T}(x::T,y::T,τ::T)
+    X = abs(x)
+    Y = abs(y)
+    D = abs(x-y)
+
+    x == y && return true
+
+    ϵ = eps(zero(x))
+    (x == 0 || y == 0 || D < ϵ) && return D < τ
+    M = typemax(typeof(x))
+    D / min(X+Y,M) < τ
+end
+
+for i in eachindex(X)
+    x = X[i]
+    I = WI.wiltonints(v1,v2,v3,x,Val{7})
+    J = dblquadints(v1,v2,v3,x,Val{7})
+    @test all(!isnan(I))
+    @test all(!isinf(I))
+    for j in eachindex(I)
+        @test nearlyequal(I[j],J[j], τ[i])
+    end
+end
+
+
+# # projection inside the triangle
+# x = (v1 + v2 + v3)/3 + 20n
+# I = WI.wiltonints(v1,v2,v3,x,Val{7})
+# J = dblquadints(v1,v2,v3,x,Val{7})
+# @test maximum(abs(I-J)./abs(J)) < 1.0e-10
+#
+# # projection outside of the triangle
+# x = (v1 + v2 + v3)/3 - 20n
+# I = WI.wiltonints(v1,v2,v3,x,Val{7})
+# J = dblquadints(v1,v2,v3,x,Val{7})
+# @test maximum(abs(I-J)./abs(J)) < 1.0e-10
+#
+# # projection on a triangle edge
+# x = (1-1.5)*v1 + 1.5*v3 + 20*n
+# I = WI.wiltonints(v1,v2,v3,x,Val{7})
+# J = dblquadints(v1,v2,v3,x,Val{7})
+# @test maximum(abs(I-J)./abs(J)) < 1.0e-10
+#
+# # projection outside of the triangle
+# x = Vec(-0.5,0.5,20.0)
+# I = WI.wiltonints(v1,v2,v3,x,Val{7})
+# J = dblquadints(v1,v2,v3,x,Val{7})
+# @test maximum(abs(I-J)./abs(J)) < 1.0e-10
+#
+# # projection on a vertex
+# x = Vec(0.0,1.0,20.0)
+# I = WI.wiltonints(v1,v2,v3,x,Val{7})
+# J = dblquadints(v1,v2,v3,x,Val{7})
+# @test maximum(abs(I-J)./abs(J)) < 1.0e-10
