76339ae3278a772bf556b929a23e9f6dd2b105d1
Kristof Cools <kristof.cools@nottingham.ac.uk>
2016-09-12 15:10:05 +0100

    allocations in auxiliary functions removed.

--- src/WiltonInts84.jl
+++ src/WiltonInts84.jl
@@ -54,65 +54,69 @@ function segints!{N}(P, Q, a, b, p, h, m, ::Type{Val{N}})
 end
 
 
-@generated function segints!{N}(P, Q, a, b, p, h, m, ::Type{Val{N}})
+@generated function segintsg{N}(a, b, p, h, m, ::Type{Val{N}})
 
-  @assert N >= 0
-  N3 = N +3
+    @assert N >= 0
+    N3 = N +3
 
-  xp = quote
+    xp = quote
 
-    h2, d = h^2, abs(h)
-    q2 = p^2+h2
-    ra, rb = sqrt(a^2+q2), sqrt(b^2+q2)
+        T = typeof(a)
+        z = zero(T)
 
-    # n = -3
-    sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
-    I1 = p == 0 ? 0 : sgn*(atan((p*b)/(q2+d*rb)) - atan((p*a)/(q2 + d*ra)))
-    j = q2 == 0 ? (b > 0 ? log(b/a) : log(a/b)) : log(b + rb) - log(a + ra)
-    J = (j,z)
-    K1 = -j
+        h2, d = h^2, abs(h)
+        q2 = p^2+h2
+        ra, rb = sqrt(a^2+q2), sqrt(b^2+q2)
 
-    j = z
-    J = (j,J1)
+        # n = -3
+        sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
+        I1 = p == 0 ? 0 : sgn*(atan((p*b)/(q2+d*rb)) - atan((p*a)/(q2 + d*ra)))
+        j = q2 == 0 ? (b > 0 ? log(b/a) : log(a/b)) : log(b + rb) - log(a + ra)
+        J = (j,z)
+        K1 = -j
 
-    # n = -1
-    I2 = p*J[2] - h*I1
-    j = (b*rb - a*ra + q2*J[2])/2
-    J = (j,J[1])
-    K2 = j
+        j = z
+        J = (j,J[1])
 
-    # n = 0
-    I3 = (b*p - a*p)/2
-    j = ((b*(b^2+q2)+2*q2*b) - (a*(a^2+q2)+2*q2*a))/3
-    J = (j,J[1])
-    K3 = j/2
+        # n = -1
+        I2 = p*J[2] - h*I1
+        j = (b*rb - a*ra + q2*J[2])/2
+        J = (j,J[1])
+        K2 = j
 
-  end
+        # n = 0
+        I3 = (b*p - a*p)/2
+        j = ((b*(b^2+q2)+2*q2*b) - (a*(a^2+q2)+2*q2*a))/3
+        J = (j,J[1])
+        K3 = j/2
 
-  for i in 4 : N3
-    Ip = symbol(:I,i-2)
-    In = symbol(:I,i)
-    Kn = symbol(:K,i)
-    it = quote
-      n = i - 3
-      $In = p/(n+2)*J[2] + n/(n+2)*h2*$Ip
-      j = (b*rb^(n+2) - a*ra^(n+2) + (n+2)*q2*J[2])/(n+3)
-      J = (j,J[1])
-      $Kn = j/(n+2)
     end
-    append!(xp.args, it.args)
-  end
 
-  xpi = Expr(:tuple)
-  xpk = Expr(:tuple)
-  for i in 1 : N3
-    push!(xpi.args, symbol(:I,i))
-    push!(xpk.args, symbol(:K,i))
-  end
+    for i in 4 : N3
+        Ip = symbol(:I,i-2)
+        In = symbol(:I,i)
+        Kn = symbol(:K,i)
+        it = quote
+            n = $i - 3
+            $In = p/(n+2)*J[2] + n/(n+2)*h2*$Ip
+            j = (b*rb^(n+2) - a*ra^(n+2) + (n+2)*q2*J[2])/(n+3)
+            J = (j,J[1])
+            $Kn = j/(n+2)
+        end
+        append!(xp.args, it.args)
+    end
 
-  push!(xp.args, :($xpi, $xpk))
+    xpi = Expr(:tuple)
+    xpk = Expr(:tuple)
+    for i in 1 : N3
+        push!(xpi.args, symbol(:I,i))
+        push!(xpk.args, symbol(:K,i))
+    end
 
-  return xp
+    push!(xp.args, :($xpi, $xpk))
+
+    @show xp
+    return xp
 end
 
 
@@ -154,6 +158,53 @@ function arcints!{N}(I, K, α, m, p, h, ::Type{Val{N}})
 end
 
 
+@generated function arcintsg{N}(α, m, p, h, ::Type{Val{N}})
+
+    xp = quote
+        T = typeof(h)
+        P = typeof(m)
+
+        h2 = h^2
+        p2 = p^2
+        q2 = h2 + p2
+        q = sqrt(q2)
+        d = sqrt(h2)
+
+        # n == -3
+        sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
+        I1 = -α * (h/q - sgn)
+        K1 = -p / q * m
+
+        # n == -1
+        I2 = α * (q - d)
+        K2 = p * q * m
+
+        # n == 0
+        I3 = α * p2 / 2
+        K3 = p * q2 / 2 * m
+    end
+
+    for i in 4 : N+3
+        Ip = symbol(:I,i-2)
+        In = symbol(:I,i)
+        Kn = symbol(:K,i)
+        b = quote
+            n = $i - 3
+            $In = α * ((n*$Ip/α + d^n)*q2 - d^(n+2)) / (n+2)
+            $Kn = p * q^(n+2) * m / (n+2)
+        end
+        append!(xp.args, b.args)
+    end
+
+    xpi = Expr(:tuple, [symbol(:I,i) for i in 1:N+3]...)
+    xpk = Expr(:tuple, [symbol(:K,i) for i in 1:N+3]...)
+    push!(xp.args, :($xpi, $xpk))
+
+    @show xp
+    return xp
+end
+
+
 function circleints!{N}(I, K, σ, p, h, ::Type{Val{N}})
 
     T = typeof(h)
@@ -185,6 +236,46 @@ function circleints!{N}(I, K, σ, p, h, ::Type{Val{N}})
 end
 
 
+@generated function circleintsg{N}(σ, p, h, ::Type{Val{N}})
+
+    xp = quote
+
+        T = typeof(h)
+
+        d = norm(h)
+        h2 = h^2
+        p2 = p^2
+        q2 = h2 + p2
+        q = sqrt(q2)
+        α = σ * 2π
+
+        # n == -3
+        sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
+        I1 = -α * (h/q - sgn)
+
+        # n == -1
+        I2 = α * (q - d)
+
+        # n == 0
+        I3 = α * p2 / 2
+    end
+
+    for i in 4 : N+3
+        Ip = symbol(:I,i-2)
+        In = symbol(:I,i)
+        b = quote
+            n = $i - 3
+            $In = α * ((n*$Ip/α + d^n)*q2 - d^(n+2)) / (n+2)
+        end
+        append!(xp.args, b.args)
+    end
+
+    push!(xp.args, Expr(:tuple, [symbol(:I,i) for i in 1:N+3]...))
+    @show xp
+    return xp
+end
+
+
 """
     angle(p,q)
 
@@ -195,6 +286,16 @@ function angle(p,q)
     cs = clamp(cs, -1, +1)
     return acos(cs)
 end
+
+
+@generated function add{N}(P::NTuple{N}, Q::NTuple{N})
+    Expr(:tuple, [:(P[$i]+Q[$i]) for i in 1:N]...)
+end
+
+@generated function add{N}(P::NTuple{N}, Q::NTuple{N}, m)
+    Expr(:tuple, [:(P[$i]+Q[$i]*m) for i in 1:N]...)
+end
+
 
 
 function wiltonints{N}(ctr, x, UB::Type{Val{N}})
@@ -218,7 +319,12 @@ function wiltonints{N}(ctr, x, UB::Type{Val{N}})
         p = dot(a-ξ,m)
         sa = dot(a-ξ,t)
         sb = dot(b-ξ,t)
-        segints!(I, K, sa, sb, p, h, m, UB)
+        # segints!(I, K, sa, sb, p, h, m, UB)
+        P, Q = segintsg(sa, sb, p, h, m, UB)
+        for i in eachindex(I); I[i] += P[i]; end
+        for i in eachindex(K); K[i] += Q[i]*m; end
+        # I = add(I, P)
+        # K = add(K, Q)
     end
 
     # arc contributions
@@ -232,14 +338,19 @@ function wiltonints{N}(ctr, x, UB::Type{Val{N}})
         ξb = b - ξ
         α = dot(ξb,u2) >= 0 ? σ*angle(ξb,u1) : σ*(angle(ξb,-u1) + π)
         m = (sin(α)*u1 + σ*(1-cos(α))*u2)
-        arcints!(I, K, α, m, p, h, UB)
+        #arcints!(I, K, α, m, p, h, UB)
+        P, Q = arcintsg(α, m, p, h, UB)
+        for i in eachindex(I); I[i] += P[i]; end
+        for i in eachindex(K); K[i] += Q[i]; end
     end
 
     # circle contributions
     for i in eachindex(ctr.circles)
         σ = ctr.circles[i]
         p = σ > 0 ? ctr.plane_outer_radius : ctr.plane_inner_radius
-        circleints!(I, K, σ, p, h, UB)
+        #circleints!(I, K, σ, p, h, UB)
+        P = circleintsg(σ, p, h, UB)
+        for i in eachindex(I); I[i] += P[i]; end
     end
 
     G[1] = K[1] - I[1]
