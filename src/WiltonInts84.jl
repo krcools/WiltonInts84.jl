a04bf1d721ac585274b1b416fd6556b666029f7d
Kristof Cools <kristof.cools@nottingham.ac.uk>
2016-09-12 18:04:13 +0100

    wiltonints made Array free

--- src/WiltonInts84.jl
+++ src/WiltonInts84.jl
@@ -4,56 +4,6 @@ export contour, wiltonints
 
 include("contour.jl")
 
-function segints!{N}(P, Q, a, b, p, h, m, ::Type{Val{N}})
-
-    @assert N ≥ 0
-    T = typeof(a)
-    z = zero(T)
-
-    h2, d = h^2, abs(h)
-    q2 = p^2+h2
-    ra, rb = sqrt(a^2+q2), sqrt(b^2+q2)
-
-    I = zeros(T, N+3)
-    K = zeros(T, N+3)
-    J = (z,z)
-
-    # n = -3
-    sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
-    I[1] = p == 0 ? 0 : sgn*(atan((p*b)/(q2+d*rb)) - atan((p*a)/(q2 + d*ra)))
-    j = q2 == 0 ? (b > 0 ? log(b/a) : log(a/b)) : log(b + rb) - log(a + ra)
-    J = (j,z)
-    K[1] = -j
-
-    j = z
-    J = (j,J[1])
-
-    # n = -1
-    I[2] = p*J[2] - h*I[1]
-    j = (b*rb - a*ra + q2*J[2])/2
-    J = (j,J[1])
-    K[2] = j
-
-    # n = 0
-    I[3] = (b*p - a*p)/2
-    j = ((b*(b^2+q2)+2*q2*b) - (a*(a^2+q2)+2*q2*a))/3
-    J = (j,J[1])
-    K[3] = j/2
-
-    # n >= 1
-    for i in 4 : length(I)
-        n = i - 3 #; j = mod1(n,2)
-        I[i] = p/(n+2)*J[2] + n/(n+2)*h2*I[i-2]
-        j = (b*rb^(n+2) - a*ra^(n+2) + (n+2)*q2*J[2])/(n+3)
-        J = (j,J[1])
-        K[i] = j/(n+2)
-    end
-
-    for k in eachindex(P) P[k] += I[k]   end
-    for k in eachindex(Q) Q[k] += K[k]*m end
-end
-
-
 @generated function segintsg{N}(a, b, p, h, m, ::Type{Val{N}})
 
     @assert N >= 0
@@ -114,47 +64,7 @@ end
     end
 
     push!(xp.args, :($xpi, $xpk))
-
-    @show xp
     return xp
-end
-
-
-function arcints!{N}(I, K, α, m, p, h, ::Type{Val{N}})
-
-    T = typeof(h)
-    P = typeof(m)
-
-    A = Vector{T}(N+3)
-    B = Vector{P}(N+3)
-
-    h2 = h^2
-    p2 = p^2
-    q2 = h2 + p2
-    q = sqrt(q2)
-    d = sqrt(h2)
-
-    # n == -3
-    sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
-    A[1] = -α * (h/q - sgn)
-    B[1] = -p / q * m
-
-    # n == -1
-    A[2] = α * (q - d)
-    B[2] = p * q * m
-
-    # n == 0
-    A[3] = α * p2 / 2
-    B[3] = p * q2 / 2 * m
-
-    for i in 4:length(A)
-        n = i-3
-        A[i] = α * ((n*A[i-2]/α + d^n)*q2 - d^(n+2)) / (n+2)
-        B[i] = p * q^(n+2) * m / (n+2)
-    end
-
-    for j in eachindex(I) I[j] += A[j]   end
-    for j in eachindex(K) K[j] += B[j]   end
 end
 
 
@@ -199,40 +109,7 @@ end
     xpi = Expr(:tuple, [symbol(:I,i) for i in 1:N+3]...)
     xpk = Expr(:tuple, [symbol(:K,i) for i in 1:N+3]...)
     push!(xp.args, :($xpi, $xpk))
-
-    @show xp
     return xp
-end
-
-
-function circleints!{N}(I, K, σ, p, h, ::Type{Val{N}})
-
-    T = typeof(h)
-    A = Vector{T}(N+3)
-
-    d = norm(h)
-    h2 = h^2
-    p2 = p^2
-    q2 = h2 + p2
-    q = sqrt(q2)
-    α = σ * 2π
-
-    # n == -3
-    sgn = norm(h) < eps(T)*1e3 ? zero(T) : sign(h)
-    A[1] = -α * (h/q - sgn)
-
-    # n == -1
-    A[2] = α * (q - d)
-
-    # n == 0
-    A[3] = α * p2 / 2
-
-    for i in 4:length(A)
-        n = i-3
-        A[i] = α * ((n*A[i-2]/α + d^n)*q2 - d^(n+2)) / (n+2)
-    end
-
-    for j in eachindex(I) I[j] += A[j]   end
 end
 
 
@@ -271,7 +148,6 @@ end
     end
 
     push!(xp.args, Expr(:tuple, [symbol(:I,i) for i in 1:N+3]...))
-    @show xp
     return xp
 end
 
@@ -296,6 +172,27 @@ end
     Expr(:tuple, [:(P[$i]+Q[$i]*m) for i in 1:N]...)
 end
 
+@generated function buildgrad{N}(I::NTuple{N}, K::NTuple{N}, h)
+    xp = quote
+        G1 = K[1] - I[1]
+    end
+
+    for i in 2:N
+        Gi = symbol(:G,i)
+        push!(xp.args, :($Gi = K[$i] - h*I[$i]))
+    end
+
+    push!(xp.args, Expr(:tuple, [symbol(:G,i) for i in 1:N]...))
+    return xp
+end
+
+@generated function maketuple{N}(T,::Type{Val{N}})
+    xp = quote
+        z = zero(T)
+    end
+    push!(xp.args, Expr(:tuple, [:z for i in 1:N+3]...))
+    xp
+end
 
 
 function wiltonints{N}(ctr, x, UB::Type{Val{N}})
@@ -305,9 +202,9 @@ function wiltonints{N}(ctr, x, UB::Type{Val{N}})
 
     ξ = x - h*n
 
-    I = zeros(eltype(x), N+3)
-    K = zeros(typeof(x), N+3)
-    G = zeros(typeof(x), N+3)
+    I = maketuple(eltype(x), UB)
+    K = maketuple(typeof(x), UB)
+    G = maketuple(typeof(x), UB)
 
     # segments contributions
     for i in eachindex(ctr.segments)
@@ -319,12 +216,9 @@ function wiltonints{N}(ctr, x, UB::Type{Val{N}})
         p = dot(a-ξ,m)
         sa = dot(a-ξ,t)
         sb = dot(b-ξ,t)
-        # segints!(I, K, sa, sb, p, h, m, UB)
         P, Q = segintsg(sa, sb, p, h, m, UB)
-        for i in eachindex(I); I[i] += P[i]; end
-        for i in eachindex(K); K[i] += Q[i]*m; end
-        # I = add(I, P)
-        # K = add(K, Q)
+        I = add(I, P)
+        K = add(K, Q, m)
     end
 
     # arc contributions
@@ -338,27 +232,20 @@ function wiltonints{N}(ctr, x, UB::Type{Val{N}})
         ξb = b - ξ
         α = dot(ξb,u2) >= 0 ? σ*angle(ξb,u1) : σ*(angle(ξb,-u1) + π)
         m = (sin(α)*u1 + σ*(1-cos(α))*u2)
-        #arcints!(I, K, α, m, p, h, UB)
         P, Q = arcintsg(α, m, p, h, UB)
-        for i in eachindex(I); I[i] += P[i]; end
-        for i in eachindex(K); K[i] += Q[i]; end
+        I = add(I, P)
+        K = add(K, Q)
     end
 
     # circle contributions
     for i in eachindex(ctr.circles)
         σ = ctr.circles[i]
         p = σ > 0 ? ctr.plane_outer_radius : ctr.plane_inner_radius
-        #circleints!(I, K, σ, p, h, UB)
         P = circleintsg(σ, p, h, UB)
-        for i in eachindex(I); I[i] += P[i]; end
+        I = add(I, P)
     end
 
-    G[1] = K[1] - I[1]
-    for i in 2:length(G)
-        G[i] = K[i] - h*I[i]
-    end
-
-    return I, K, G
+    return I, K, buildgrad(I, K, h)
 end
 
 """
